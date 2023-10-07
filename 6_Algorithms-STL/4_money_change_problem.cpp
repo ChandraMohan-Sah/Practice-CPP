f7615f9ea1198632faaed13f32ba0e6bfb24143f
ChandraMohan-Sah <csah9628@gmail.com>
2023-10-07 18:30:39 +0545

    ok

--- 6_Algorithms-STL/4_money_change_problem.cpp
+++ 6_Algorithms-STL/4_money_change_problem.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
+bool compare (int a , int b){
+    return a<=b;
+}
+
 int main()
-{
-    cout<<"Hello Worls";
+{   
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    int n = sizeof(coins)/sizeof(int);
+
+    int money ;
+    cin >> money;
+
+    while(money > 0){
+        int index = lower_bound(coins, coins+n, money, compare ) - coins -1; 
+        cout<<"We would give exchange of coins "<< coins[index] <<endl;
+        money = money - coins[index];    
+    }
+
     return 0;
 }
+
